be9a84fa9cac43da37d46f1ed1d6de981534365f
flyreh <jose.manusc12224@gmail.com>
2024-04-27 03:25:57 -0500

    init

---
+++ src/main/NewClass.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NewClass extends JPanel {
+
+    public NewClass(){
+        this.setLayout(null);
+        this.setBounds(100, 100, 30, 30);
+        this.setVisible(true);
+        this.setBackground(Color.BLACK);
+        initComponets();
+
+    }
+
+    public void initComponets(){
+
+    }
+    
+}
---
+++ src/main/Ventana.java
@@ -0,0 +1,27 @@
+package main;
+
+public class Ventana extends javax.swing.JFrame{
+
+
+    public NewClass mipanel;
+
+    public Ventana(){
+        initComponents();
+    }
+
+    public void initComponents(){
+
+        mipanel = new NewClass();
+
+        this.setBounds(100, 100, 500, 500);
+        this.setLayout(null);
+
+        this.add(mipanel);
+    }
+
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+    }
+
+}
